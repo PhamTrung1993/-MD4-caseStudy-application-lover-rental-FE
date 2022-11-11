24b4a5460fb616f59af20efc8e1e309a87084be4
quanph14 <phquan.0493@gmail.com>
2022-11-11 17:29:18 +0700

    done login and token, doing roling

--- caseStudy4-application-lover-rental-FE/login/login.js
+++ caseStudy4-application-lover-rental-FE/login/login.js
@@ -1,26 +1,69 @@
-function login() {
-    event.preventDefault();
-    //lay du lieu
-    //lay username, password
-    let name = $("#name").val();
-    let password = $("#password").val();
-    //tao object
-    let user ={
-        "name": name,
-        "password": password
-    }
-    $.ajax({
-        headers:{
-            'Accept': 'application/json',
-            'Content-Type': 'application/json'
-        },
-        type: "POST",
-        url: "http://localhost:8080/login",
-        data: JSON.stringify(user),
-        success: function (data) {
-            localStorage.setItem("ob", JSON.stringify(data));
-            window.location.href = "../admin/admin.html";
-        }
-    })
+let currentUser = JSON.parse(localStorage.getItem("currentUser"));
+let token = localStorage.getItem("token");
+console.log(currentUser)
 
+function checkLogin() {
+    if (currentUser === null) {
+        $('#dropdownMenuButton1').hide();
+        $('#profile').hide();
+    }
+    else {
+        document.getElementById("admin-name").innerHTML = currentUser.username;
+        $('#dropdownMenuButton').hide();
+    }
+}
+
+checkLogin();
+
+function logout() {
+    localStorage.clear();
+    window.location.href = "home.html";
+}
+
+function login() {
+    let username = $('#username').val();
+    let password = $('#password').val();
+    if (username === "") {
+        document.getElementById("error_login").innerHTML = "username address cannot be blank !";
+        return false;
+    }
+    if (password === "") {
+        document.getElementById("error_login").innerHTML = "Password can not be blank !";
+        return false;
+    }
+    let data = {
+        userName: username,
+        password: password,
+    };
+    $.ajax({
+        url: `http://localhost:8080/login`,
+        type: 'POST',
+        headers: {
+            'Accept': 'application/json',
+            'Content-type': 'application/json'
+        },
+        data: JSON.stringify(data),
+        success: function (data) {
+            localStorage.setItem("token", data.accessToken);
+            localStorage.setItem("role",data.roles[0].authority);
+
+            if (data.roles[0].authority == "ROLE_PO"){
+                location.href = "../po/po.html"
+            }else if(data.roles[0].authority == "ROLE_ADMIN"){
+                location.href = "../admin/admin.html"
+            }else if(data.roles[0].authority == "ROLE_PROVIDER"){
+                location.href = "../provider/provider.html"
+            }else if(data.roles[0].authority == "ROLE_USER"){
+                location.href = "../user/user.html"
+            }else {
+                location.href = "login.html"
+            }
+            console.log(data)
+        },
+        error: function (err) {
+            alert("Đăng nhập không thành công")
+            console.log(err)
+        }
+    });
+    event.preventDefault();
 }
