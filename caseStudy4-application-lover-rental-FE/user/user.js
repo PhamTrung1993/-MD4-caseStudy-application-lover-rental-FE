56645e9afd70424af3b1e8afc0fc4f2086be7ad7
hoang4963 <hoangth.finbox@gmail.com>
2022-11-14 03:54:29 +0700

    done user

--- caseStudy4-application-lover-rental-FE/user/user.js
+++ caseStudy4-application-lover-rental-FE/user/user.js
@@ -1,37 +1,119 @@
-
-function getAllUser(){
+successHandler();
+function successHandler() {
     $.ajax({
         type: "GET",
-        url: "http://localhost:8080/user/" + id,
-        success: function (data){
-            showAllUser(data)
+        //tên API
+        url: "http://localhost:8080/user/lists",
+        //xử lý khi thành công
+        success: function (data) {
+            // hien thi danh sach o day
+            let content = '    <tr>\n' +
+                '        <td>ID</td>\n' +
+                '        <td>Name</td>\n' +
+                '        <td>Email</td>\n' +
+                '        <td>Delete</td>\n' +
+                '        <td>Become Provider</td>\n' +
+                '    </tr>';
+            for (let i = 0; i < data.length; i++) {
+                content += getUser(data[i]);
+            }
+            document.getElementById('userList').innerHTML = content;
         }
-    })
+    });
+}
+function getUser(user) {
+    return `<tr><td >${user.id}</td><td >${user.userName}</td><td >${user.email}</td>` +
+        `<td><button onclick='deleteUser(${user.id})'">Delete</button></td>` +
+        `<td><button onclick='showFormAddProvider(${user.id})'">Become Provider</button></td>` +
+        `</tr>`;
+}
+function deleteUser(id){
+    $.ajax({
+        type: "DELETE",
+        //tên API
+        url: "http://localhost:8080/user/" +id,
+        //xử lý khi thành công
+        success: function (data) {
+            alert("done");
+            successHandler();
+        }
+    });
 }
 
-function showAllUser(user){
-    let res = "";
-        let user1 = user
-        res =  `<li className="item">
-                    <h2 className="product-name"><a title=" Sample Product" href="#"> ${user1.userName} </a></h2>
-                    </div>
-                    <div>
-                    <tr>
-                    <td>Nick Name:${user1.userName}</td>
-                    <td>Email : ${user1.email}</td>
-                    <td>Phone : ${user1.phone}</td>
-                    <td>Ngay gia nhap : ${user1.joinDate}</td> 
-                    <td>VIP : ${user1.vip}</td>
-                    </tr>
-                
-                        </div>
-                  
-                </div>
-            </div>
-        </li>`;
-
-    document.getElementById("view").innerHTML = res  ;
+function showFormAddUser(){
+    let form = "<tr>\n" +
+        "            <td>userName:</td>\n" +
+        "            <td><input type=\"text\" id=\"userName\" placeholder=\"Username\"></td>\n" +
+        "        </tr>\n" +
+        "        <tr>\n" +
+        "            <td>Password:</td>\n" +
+        "            <td><input type=\"password\" id=\"password\" placeholder=\"Password\"></td>\n" +
+        "        </tr>\n" +
+        "        <tr>\n" +
+        "            <td>Email:</td>\n" +
+        "            <td><input type=\"email\" id=\"email\" placeholder=\"Email\"></td>\n" +
+        "        </tr>\n" +
+        "        <tr>\n" +
+        "            <td>Phone:</td>\n" +
+        "            <td><input type=\"text\" id=\"phone\" placeholder=\"Phone\"></td>\n" +
+        "        </tr>\n" +
+        "        <tr>\n" +
+        "            <td>Vip:</td>\n" +
+        "            <td><input type=\"text\" id=\"vip\" placeholder=\"Vip\"></td>\n" +
+        "        </tr>\n" +
+        "        <tr>\n" +
+        "            <td>JoinDate:</td>\n" +
+        "            <td><input type=\"date\" id=\"date\" placeholder=\"Join Date\"></td>\n" +
+        "        </tr>\n" +
+        "        <tr>\n" +
+        "            <td>Role:</td>\n" +
+        "            <td><select id=\"role\">\n" +
+        "            <option value=\"1\">PO</option>\n" +
+        "            <option value=\"2\">ADMIN</option>\n" +
+        "            <option value=\"3\">USER</option>\n" +
+        "            <option value=\"4\">PROVIDER</option>\n" +
+        "            </select></td>\n" +
+        "        </tr>\n" +
+        "        <tr>\n" +
+        "            <td></td>\n" +
+        "            <td><input type=\"submit\" value=\"Add\" onclick=\"addNewUser()\"></td>\n" +
+        "        </tr>";
+    document.getElementById("userList").innerHTML = form;
 }
-function loginSuccess(){
-    localStorage.getItem()
+function addNewUser(){
+    let userName = $('#userName').val();
+    let password = $('#password').val();
+    let email = $('#email').val();
+    let phone = $('#phone').val();
+    let joinDate = $('#date').val();
+    let vip = $('#vip').val();
+    let role = $('#role').val();
+    let newUser = {
+        userName: userName,
+        password: password,
+        email: email,
+        phone: phone,
+        joinDAte: joinDate,
+        roleId : role,
+        vip: vip
+    };
+    // goi ajax
+    $.ajax({
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+        type: "POST",
+        data: JSON.stringify(newUser),
+        //tên API
+        url: "http://localhost:8080/user",
+        //xử lý khi thành công
+        success: function (){
+            alert("done");
+            successHandler()
+        }
+
+    });
+    //chặn sự kiện mặc định của thẻ
+    event.preventDefault();
 }
