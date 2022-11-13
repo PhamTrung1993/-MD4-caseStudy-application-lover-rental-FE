9e19562c646e506b2643ae48a726447c7e394866
Duonghieu97st <Duonghieu97st@gmail.com>
2022-11-13 14:58:26 +0700

    update show all order

--- caseStudy4-application-lover-rental-FE/order/order.js
+++ caseStudy4-application-lover-rental-FE/order/order.js
@@ -1,15 +1,35 @@
 token = localStorage.getItem("token");
 let id = JSON.parse(localStorage.getItem("currentUser")).id;
-showOrder();
+console.log(currentUser)
+// showAllOrder();
 
-function showOrder() {
+function showAllOrder() {
     $.ajax({
         headers: {
             'Accept': 'application/json', 'Content-Type': 'application/json'
         }, beforeSend: function (xhr) {
             xhr.setRequestHeader("Authorization", "Bearer " + token);
-        }, type: "get", url: "http://localhost:8080/orders/" + id, success: function (data) {
-            let userName = `<h2>` + data.user.name + `</h2>`
+        }, type: "get", url: "http://localhost:8080/orders/",
+        success: function (data) {
+            showAllOrder()
         }
     })
+}
+function showAllOrder(lists) {
+    let res = "<tr>\n" +
+        "    <td>STT</td>\n" +
+        "    <td>Name User</td>\n" +
+        "    <td>Name Provider</td>\n" +
+        "    <td>Date</td>\n" +
+        "    <td>Status</td>\n" +
+        "</tr>\n";
+    for (let i= 0; i< lists.length; i++) {
+        let order = lists[i];
+        res += "<tr>\n" +
+            "    <td>" + (i +1)       +   "</td>\n" +
+            "    <td>" + order.user_id  + "</td>\n" +
+
+            `<td><a class="deleteOrder" onclick='deleteOrderById($(this))' href="${order.id}">Delete</a></td></tr>`;
+    }
+
 }
